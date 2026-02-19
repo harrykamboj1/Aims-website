74c4ffb2eb9472b284fc201b2fe9c6706c11dcac
harrykamboj1 <singhharnoor116@gmail.com>
2026-02-19 07:42:32 +0530

    chore: Commented out the ContactForm component on the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
       <Services />
       <Gallery />
       <Testimonial />
-      <ContactForm />
+      {/* <ContactForm /> */}
       <Footer />
     </main>
   );
